a2e44c9ecb079bb11268b525105cdea148a312df
balay <dlolhd@gmail.com>
2022-11-21 08:32:22 +0100

    Creating a modifier. The following addition to the modifier will draw a black, 2dp wide border around a composable.

--- app/src/main/java/com/example/modifierdemo/MainActivity.kt
+++ app/src/main/java/com/example/modifierdemo/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.modifierdemo
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.modifierdemo.ui.theme.ModifierDemoTheme
 
@@ -33,8 +37,12 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun DemoScreen() {
+    val modifier = Modifier
+        .border(width = 2.dp, color = Color.Black)
+        .padding(all = 10.dp)
     Text(
         text = "Hello Compose",
+        modifier = modifier,
         fontSize = 40.sp,
         fontWeight = FontWeight.Bold
     )
